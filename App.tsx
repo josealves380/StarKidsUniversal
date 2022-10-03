f87dbcdac31723605534cf987f40348369e24886
Jose <netopereira464@gmail.com>
2022-10-02 21:58:11 -0300

    Image for readme

--- App.tsx
+++ App.tsx
@@ -1,6 +1,6 @@
 
 import { StatusBar } from 'react-native';
-import { Background } from './src/components/Star/Background';
+import { Background } from './src/components/Background/Background';
 
 export default function App() {
   return (
